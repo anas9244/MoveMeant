abcab0bd813da1c0cfb65ca66927fa63b71675c0
Anas Alnayef <anas9244@hotmail.com>
2017-08-12 19:40:37 +0200

    NumOf visits

--- php script/NewPlace.php
+++ php script/NewPlace.php
@@ -1,6 +1,6 @@
 <?php 
 require_once('../mysql_connect.php');
-$place_id=$_POST['place_id'];
+$place_id = $_POST['place_id'];
 
 $user_id=$_POST['user_id'];
 
@@ -16,7 +16,7 @@ $resultVisit_qry=mysqli_query($dbc ,$checkVisit_qry);
 if(mysqli_num_rows($resultVisit_qry) == 0)
 {
 
-		$insertVisit_qry = "insert into places_users (user_id,place_id) values ('$user_id','$place_id')";
+		$insertVisit_qry = "insert into places_users (user_id,place_id,last_visit) values ('$user_id','$place_id',now())";
 
 		if($dbc->query($insertVisit_qry)===TRUE) {
 
@@ -35,6 +35,30 @@ if(mysqli_num_rows($resultVisit_qry) == 0)
 	else {
 	echo  "error new visit" . $insertVisit_qry . "<br>" . $dbc->error ;
 	}
+}
+else
+{
+$newVisit_qry="CALL NewVisit('$place_id','$user_id');";
+
+if($dbc->query($newVisit_qry)===TRUE) {
+
+			$checkNewVisit_qry= "select * from places_users where user_id like '$user_id' and place_id like '$place_id';";
+			$resultNewVisit_qry=mysqli_query($dbc ,$checkNewVisit_qry);
+
+			
+			while($row = mysqli_fetch_assoc($resultNewVisit_qry))
+			{
+				$output[]=$row;
+			}
+	
+
+			print(json_encode($output));
+	}
+	else {
+	echo  "error new visit" . $insertVisit_qry . "<br>" . $dbc->error ;
+	}
+
+
 }
 mysqli_close($dbc);
 
